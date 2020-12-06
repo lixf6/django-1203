5871958a51783d6178398a256951f13b2539e1db
Li Xuefu 李学富 <LIXF6@cn.midea.com>
2020-12-06 11:25:27 +0800

    注册功能

--- mydjpro/Scripts/mysite/account/urls.py
+++ mydjpro/Scripts/mysite/account/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('login/', views.user_login, name='user_login'),
     path('login/', auth_views.LoginView.as_view(template_name='account/login2.html'), name='user_login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='account/logout.html'), name='user_logout'),
+    path('register/', views.register, name='user_register'),
 ]
--- mydjpro/Scripts/mysite/account/views.py
+++ mydjpro/Scripts/mysite/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm, UserProfileForm
 
 
 def user_login(request):
@@ -20,3 +20,24 @@ def user_login(request):
                 return HttpResponse('Sorry, Your username or password is not right.')
         else:
             return HttpResponse('Invalid login.')
+
+
+def register(request):
+    if request.method == 'GET':
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()   # new
+        return render(request, 'account/register.html', {'form': user_form, 'profile': userprofile_form})   # new
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)  # new
+        if user_form.is_valid() * userprofile_form.is_valid():   # new
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            # new
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            return HttpResponse('恭喜，注册成功了。')
+        else:
+            return HttpResponse('Sorry，注册失败了。')
